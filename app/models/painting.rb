deb4f1443b898184ea95b98c813c835875706d9e
github username <github email address>
2019-05-03 09:08:13 -0700

    All but most_prolific

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,25 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, artist, gallery, price)
     @title = title
     @price = price
+    @gallery = gallery
+    @artist = artist
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    self.all.map{|paintings|
+      paintings.price
+    }.sum
   end
 
 end
