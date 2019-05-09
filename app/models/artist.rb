9584141cabfb8433588b1d90ac745f80529d710c
Greg Morgan <gregory.morgan@flatironschool.com>
2019-05-09 13:12:03 -0700

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,55 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all=[]
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def create_painting(gallery, price, title)
+    Painting.new(self, gallery, price, title)
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+    # returns an array of every painting object by an artist
+  end
+
+  def galleries
+    paintings.map do |painting|
+      painting.gallery
+    end
+    # returns an array of every painting object in this gallery
+  end
+
+  def cities
+    galleries.map do |gallery|
+      gallery.city
+    end
+    # returns an array of just city names
+  end
+
+  def self.total_experience
+    self.all.map do |artist|
+      artist.years_experience
+    end.sum
+    # returns just an integer
+  end
+
+  def self.most_prolific
+    self.all.max_by do |artist|
+      artist.paintings.length/artist.years_experience
+    end
+    # returns one artist object
   end
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,44 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all =[]
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.gallery == self
+    end
+    # returns an array of every painting Object in this gallery
+  end
+
+  def artists
+    paintings.map do |painting|
+      painting.artist
+    end
+    # returns an array of every artist object in this gallery
+  end
+
+  def artist_names
+    artists.map do |artist|
+      artist.name
+    end
+    # returns an array of just the names of the artists in this gallery
+  end
+
+  def most_expensive_painting
+    paintings.max_by do |painting|
+      painting.price
+    end
+    # returns one (not array) painting object of the most expensice painting in the gallery.
   end
 
 end
